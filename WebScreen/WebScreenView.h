e2faf4ebbcbdc3d61c6e2f40d4dfc563a7857476
Yusaku Matsuki <ymatzki@gmail.com>
2019-11-11 22:20:45 +0900

    Initial Commit

---
+++ WebScreen/WebScreenView.h
@@ -0,0 +1,13 @@
+//
+//  WebScreenView.h
+//  WebScreen
+//
+//  Created by 松木優作 on 2019/11/11.
+//  Copyright © 2019 YusakuMatsuki. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface WebScreenView : ScreenSaverView
+
+@end
